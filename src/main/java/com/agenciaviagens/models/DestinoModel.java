8930df3fef391776e2f2224aa2ba69883ef62f30
igorperonico <igormtpr@gmail.com>
2024-08-09 15:09:40 -0300

    feat: add validations for name and country fields to DestinoModel

--- src/main/java/com/agenciaviagens/models/DestinoModel.java
+++ src/main/java/com/agenciaviagens/models/DestinoModel.java
@@ -2,6 +2,7 @@ package com.agenciaviagens.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.List;
@@ -17,11 +18,14 @@ public class DestinoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "O nome do destino é obrigatório.")
     private String nome;
+
+    @NotBlank(message = "O país é obrigatório.")
     private String pais;
 
     @JsonBackReference
     @OneToMany(mappedBy = "destino")
     private List<ViagemModel> viagens;
-
 }
