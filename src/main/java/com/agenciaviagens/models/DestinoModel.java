0c11faf197c805aa9fae0e66c7d915332f10cabe
igorperonico <igormtpr@gmail.com>
2024-08-08 21:43:30 -0300

    feat: create DestinoModel entity

---
+++ src/main/java/com/agenciaviagens/models/DestinoModel.java
@@ -0,0 +1,27 @@
+package com.agenciaviagens.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "destino")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class DestinoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String pais;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "destino")
+    private List<ViagemModel> viagens;
+
+}
