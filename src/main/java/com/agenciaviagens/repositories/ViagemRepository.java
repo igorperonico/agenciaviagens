03cb7cb120b6238a5e7ce0766402fa3b613e9dbe
igorperonico <igormtpr@gmail.com>
2024-08-09 14:29:47 -0300

    feat: add methods to filter trips by destination, date range, and minimum value

--- src/main/java/com/agenciaviagens/repositories/ViagemRepository.java
+++ src/main/java/com/agenciaviagens/repositories/ViagemRepository.java
@@ -4,6 +4,13 @@ import com.agenciaviagens.models.ViagemModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface ViagemRepository extends JpaRepository<ViagemModel, Long> {
+    List<ViagemModel> findByDestinoId(Long destinoId);
+    List<ViagemModel> findAllByDataSaidaBetween(String dataSaida1, String dataSaida2);
+    List<ViagemModel> findAllByValorTotalGreaterThan(BigDecimal valorTotal);
+
 }
